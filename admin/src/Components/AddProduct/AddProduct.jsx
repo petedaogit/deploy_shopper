c5b6706fa547460c6b5720dab2f3b38dcc765f12
petedaogit <namdao.0908@gmail.com>
2024-08-24 17:17:17 +0700

    addproduct update

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -11,6 +11,7 @@ function AddProduct() {
     new_price: "",
     old_price: "",
   });
+  const deploymentUrl = "https://deploy-shopper.onrender.com";
 
   const imageHandler = (e) => {
     setImage(e.target.files[0]);
@@ -26,7 +27,7 @@ function AddProduct() {
     let formData = new FormData();
     formData.append("product", image);
 
-    await fetch("https://deploy-shopper.onrender.com/upload", {
+    await fetch(`${deploymentUrl}/upload`, {
       method: "POST",
       headers: {
         Accept: "application/json",
@@ -38,7 +39,7 @@ function AddProduct() {
         responseData = data;
       });
     if (responseData.success) {
-      product.image = responseData.image_url;
+      product.image = `${deploymentUrl}/images/${responseData.image_filename}`;
       console.log(product);
       await fetch("https://deploy-shopper.onrender.com/addproduct", {
         method: "POST",
