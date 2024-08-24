0101ea49ed18d06af88818c2ec315a89df2052de
petedaogit <143330101+petedaogit@users.noreply.github.com>
2024-08-24 13:08:11 +0700

    Update Popular.jsx

--- my-app/src/Components/Popular/Popular.jsx
+++ my-app/src/Components/Popular/Popular.jsx
@@ -6,7 +6,7 @@ function Popular() {
   const [popularProducts, setPopularProducts] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:4000/popularinwomen")
+    fetch("https://deploy-shopper.onrender.com/popularinwomen")
       .then((response) => response.json())
       .then((data) => setPopularProducts(data));
   }, []);
