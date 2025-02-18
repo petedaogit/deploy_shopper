8cae8b8f5d68473c6d6b5bcafb34fd7170674d86
petedaogit <143330101+petedaogit@users.noreply.github.com>
2025-02-18 16:38:35 +0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://namdao0908:admin123@cluster0.8vwbb.mongodb.net/e-commerce"
+      "mongodb+srv://namdao0908:vIvXvwBkMepBkeow@cluster0.8vwbb.mongodb.net/e-commerce"
     )
     .then(() => console.log("DB Connected!"));
 };
